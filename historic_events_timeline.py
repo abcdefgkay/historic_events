f1a969158424cf59a96bcd58d8f247c9a8460cfc
Robert <Sokvoidj@protonmail.com>
2025-06-27 13:55:22 +0200

    Update historic_events_timeline.py

--- historic_events_timeline.py
+++ historic_events_timeline.py
@@ -12,7 +12,7 @@ events = [
 
 # Function to display the historical timeline
 def display (events):
-    if events is true
+    if events is false
     
 def display_timeline(events):
     print("Historic Events Timeline:")
